966e91d090ca78f979270142566a083fc09ef3c1
Adri Torres <adriana.amaral@msn.com>
2020-11-08 18:50:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,10 @@
 
 
-def reverse_each_word
-  print reverse_each_word.split(" ")
-  reverse_each_word.each do |words|
-   words.reverse
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
   end
- 
+  test_array.join(" ")
 end
