bba0c50f742e7dc29c51625561a15bd83ae71a0e
Adri Torres <adriana.amaral@msn.com>
2020-11-08 18:46:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,10 @@
 
 def reverse_each_word
   print reverse_each_word.split(" ")
-  
+  return_array = []
+  reverse_each_word.each do |words|
+    return_array << words.reverse
+  end
+  return_array.join(" ")
+
 end
