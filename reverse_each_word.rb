67786c391c0e2250b405b3199a279acc9d7dd0ea
Adri Torres <adriana.amaral@msn.com>
2020-11-08 18:59:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,5 +6,5 @@ def reverse_each_word(sentence)
   turning_string_into_array.collect do|string| 
     my_new_array << string.reverse 
   end
-  turning_string_into_array.join(" ")
+  my_new_array.join(" ")
 end
